08ee2de7699a64d7f10f9404e6c80bd7fa30d202
LiamOSullivan <lmosulli@tcd.ie>
2020-06-30 22:00:30 +0100

    list blobs

--- bulk-archive-blob.js
+++ bulk-archive-blob.js
@@ -10,30 +10,22 @@ async function main () {
   console.log('Bulk archive service started ' + d.toString().split('G')[0])
 
   // Create the BlobServiceClient object which will be used to create a container client
-  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
-
-  // Create a unique name for the container
-  // const containerName = 'quickstart' + uuidv1();
-
-  // console.log('\nCreating container...');
-  // console.log('\t', containerName);
-
+  const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING)
   // Get a reference to a container
   const containerClient = blobServiceClient.getContainerClient('luas-data')
-
-  // Create the container
-  // const createContainerResponse = await containerClient.create()
-  // console.log('Container was created successfully. requestId: ', createContainerResponse.requestId)
-
-
   console.log('\nListing blobs...')
-
-
   // List the blob(s) in the container.
   for await (const blob of containerClient.listBlobsFlat()) {
     console.log('\t', blob.name)
   }
 
+  // Create a unique name for the container
+  // const containerName = 'quickstart' + uuidv1();
+  // console.log('\nCreating container...');
+  // console.log('\t', containerName);
+  // Create the container
+  // const createContainerResponse = await containerClient.create()
+  // console.log('Container was created successfully. requestId: ', createContainerResponse.requestId)
 
   d = new Date()
   console.log('Bulk archive service stopped ' + d.toString().split('G')[0])
